295fd9bdf23f20ae8f7c4f9ceba6d60340feb4d0
caisongjie <caisongjie331@163.com>
2018-03-28 15:58:31 -0700

    create dataset and unit testing

--- LinearRegression/LR_buildFromScratch.py
+++ LinearRegression/LR_buildFromScratch.py
@@ -2,10 +2,25 @@ from statistics import mean
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import style
+import random
 
 style.use('fivethirtyeight')
-xs = np.array([1,2,3,4,5,6], dtype=np.float64)
-ys = np.array([5,4,6,5,6,7], dtype=np.float64)
+# xs = np.array([1,2,3,4,5,6], dtype=np.float64)
+# ys = np.array([5,4,6,5,6,7], dtype=np.float64)
+
+def create_dataset(data_num,variance, step=2, correlation=False):
+    val = 1
+    ys = []
+    for i in range(data_num):
+        y = val + random.randrange(-variance, variance)
+        ys.append(y)
+        if correlation and correlation =='pos':
+            val += step
+        elif correlation and correlation =='neg':
+            val -= step
+    xs = [ i for i in range(len(ys))]
+
+    return np.array(xs, dtype=np.float64), np.array(ys, dtype=np.float64)
 
 def best_fit_slope_and_intercept(xs, ys):
     x_bar = np.mean(xs)
@@ -26,7 +41,7 @@ def coefficient_of_determination(ys_orig, ys_line):
     squared_error_y_mean = squared_error(ys_orig, y_mean)
     return 1 - (squared_error_reg / squared_error_y_mean)
 
-
+xs, ys = create_dataset(40, 20, 2, correlation='pos')
 
 m,b = best_fit_slope_and_intercept(xs,ys)
 regression_line = [(m*x)+b for x in xs]
@@ -38,7 +53,7 @@ r_squared = coefficient_of_determination(ys,regression_line)
 
 print(r_squared)
 plt.scatter(xs,ys)
-plt.scatter(predict_x,predict_y, color='g')
+plt.scatter(predict_x,predict_y, s=100, color='g')
 plt.plot(xs, regression_line)
 plt.show()
 
