ae137e485a243b55c6d2da6b9de18eccbcb1abb4
caisongjie <caisongjie331@163.com>
2018-03-30 01:12:05 -0700

    first try with support vector machine algorithm

---
+++ Classification/svm.py
@@ -0,0 +1,24 @@
+import numpy as np
+from sklearn import preprocessing, cross_validation, neighbors, svm
+import pandas as pd
+
+df = pd.read_csv('breast-cancer-wisconsin.data.txt')
+df.replace('?',-99999,inplace=True)# deal with missing data
+df.drop(['id'],1,inplace=True)#remove the id column
+
+x = np.array(df.drop(['class'],1))
+y = np.array(df['class'])
+
+x_train, x_test, y_train, y_test = cross_validation.train_test_split(x,y,test_size=0.2)
+
+clf = svm.SVC()
+clf.fit(x_train,y_train)
+
+accuracy = clf.score(x_test,y_test)
+print(accuracy)
+
+example_measures = np.array([[4,2,1,1,1,2,3,2,1],[4,2,1,2,2,2,3,2,1]])
+example_measures = example_measures.reshape(len(example_measures),-1)
+predict_val = clf.predict(example_measures)
+print(predict_val)
+
